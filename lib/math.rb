4976abeb63e74a174e56f9d8e0cb87dc71652753
Michael Sotir <sotirm@umich.edu>
2020-04-29 00:21:52 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,33 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  sum
 end
 
 def subtraction(num1, num2)
-
+  subtract_num = num1 - num2
+  subtract_num
 end
 
 def division(num1, num2)
+  div_final = num1 / num2
+  div_final
 
 end
 
 def multiplication(num1, num2)
+  mult_final = num1 * num2
+  mult_final
 
 end
 
 def modulo(num1, num2)
+  mod_final = num1 % num2
+  mod_final
 
 end
 
 def square_root(num)
+  final_root = Math.sqrt(num)
+  final_root
 
 end
